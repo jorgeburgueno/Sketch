654ebba8eddac541acfadfbc828bdfccb3ebf263
Jorge Burgueno <jorge.albert16@hotmail.com>
2023-04-20 19:53:01 -0700

    creador promp

---
+++ script.js
@@ -0,0 +1,7 @@
+let numeroDiv = prompt("Cuantas celda quieres?");
+
+for (i = 0; i < numeroDiv; i++ ) {
+
+let div = document.createElement ("div");
+document.getElementById("main").appendChild(div);
+}
